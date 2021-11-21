7ea1e0ac7e4a593d58bbe350f9dbd26ddddddd6b
DogukanGun <dogukangundogan5@gmail.com>
2021-11-21 19:14:21 +0300

    Corousel is fixed

--- src/app/pages/corousel-slider/corousel-slider.component.ts
+++ src/app/pages/corousel-slider/corousel-slider.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
+import { NgbCarouselConfig,NgbCarousel , NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-corousel-slider',
   templateUrl: './corousel-slider.component.html',
-  styleUrls: ['./corousel-slider.component.scss']
+  styleUrls: ['./corousel-slider.component.scss'],
 })
 export class CorouselSliderComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('carousel', { static: true })
+  carousel!: NgbCarousel;
 
+  paused = false;
+  unpauseOnArrow = false;
+  pauseOnIndicator = false;
+  pauseOnHover = true;
+  pauseOnFocus = true;
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 10000;
+    config.wrap = false;
+  
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
   ngOnInit(): void {
   }
 
+  togglePaused() {
+    if (this.paused) {
+      this.carousel.cycle();
+    } else {
+      this.carousel.pause();
+    }
+    this.paused = !this.paused;
+  }
+
+  onSlide(slideEvent: NgbSlideEvent) {
+    if (this.unpauseOnArrow && slideEvent.paused &&
+      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT || slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)) {
+      this.togglePaused();
+    }
+    if (this.pauseOnIndicator && !slideEvent.paused && slideEvent.source === NgbSlideEventSource.INDICATOR) {
+      this.togglePaused();
+    }
+  }
 }
