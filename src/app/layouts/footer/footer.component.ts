7d7bf5764a886a2f918faa09a7f3b75350d89837
alpertml <alper.tml.14@hotmail.com>
2021-12-19 00:08:05 +0300

    some improvements accomplished

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  footer_elements = ['Home','Pricing','Privacy','Cookies','About','Contact']
   constructor() { }
 
   ngOnInit(): void {
